9149b8c74435e610f4ad72b528ee85f2c9457422
Dhanush <dhanushxenocrate@gmail.com>
2022-07-19 08:11:32 +0000

    Sync LeetCode submission - Search Insert Position (javascript)

---
+++ problems/search_insert_position/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let result = nums.indexOf(target)
+    let i = 0;
+    const numLength = nums.length;
+    if(!(result>=0)){
+        while(i<=numLength){
+            if(nums[i]<target){
+                ++i;    
+            }
+            else{
+                result = nums.indexOf(nums[i])
+                result = result >=0 ? result : numLength
+                break;
+            }
+        }
+    }
+    return result;
+};
