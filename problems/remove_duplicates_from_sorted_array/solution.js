45c9e16443a024719868dcac28e6f29aca82944a
Dhanush <dhanushxenocrate@gmail.com>
2022-11-11 10:00:10 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array (javascript)

---
+++ problems/remove_duplicates_from_sorted_array/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let insertIndex = 1;
+    for(let i=1;i<nums.length;i++){
+        if(nums[i-1] != nums[i]){
+            nums[insertIndex] = nums[i]
+            insertIndex++;
+        }
+    }
+    return insertIndex;
+}
