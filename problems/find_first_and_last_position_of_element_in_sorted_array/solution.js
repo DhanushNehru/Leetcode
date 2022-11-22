78a146954f7ef15c7c93caed6c7059f0c28d5e94
Dhanush <dhanushxenocrate@gmail.com>
2022-11-22 15:55:27 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (javascript)

--- problems/find_first_and_last_position_of_element_in_sorted_array/solution.js
+++ problems/find_first_and_last_position_of_element_in_sorted_array/solution.js
@@ -4,7 +4,31 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    const starting = nums.indexOf(target);
-    const ending = nums.lastIndexOf(target);
-    return [starting, ending]
+    return [occurance(nums, target, true),occurance(nums, target, false)]
 };
+
+const occurance = (nums, target, isFirstOccurance) => {
+    let left = 0;
+    let right = nums.length - 1;
+    let ans = -1
+    let mid;
+    while(left <= right){
+       mid = Math.floor((right+left)/2)
+       if(nums[mid]==target){
+           if(isFirstOccurance){
+               right = mid - 1
+           }
+           else{
+               left = mid + 1
+           }
+           
+           ans = mid;
+
+       }else if(nums[mid]<target){
+           left = mid + 1
+       }else{
+           right = mid -1
+       }
+    }
+    return ans
+}
