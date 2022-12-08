8805e85ce7fb5ee1bc6b8297a5e4e9d5733a9f93
Dhanush <dhanushxenocrate@gmail.com>
2022-12-08 16:29:05 +0000

    Sync LeetCode submission - Pow(x, n) (javascript)

--- problems/pow(x,_n)/solution.js
+++ problems/pow(x,_n)/solution.js
@@ -4,5 +4,11 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-    return Math.pow(x,n)
+    if (n===0) return 1;
+    
+    const pow = Math.abs(n);
+    
+	const result = pow%2===0 ? myPow(x*x,pow/2) : myPow(x*x,(pow-1)/2) * x;
+    
+    return n < 0 ? 1/result : result;
 };
