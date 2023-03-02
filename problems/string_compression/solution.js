74ffe724bca3be21671c843030f03c77700b00fd
Dhanush <dhanushxenocrate@gmail.com>
2023-03-02 07:51:10 +0000

    Sync LeetCode submission - String Compression (javascript)

---
+++ problems/string_compression/solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+  if (!chars.length) return 0;
+  let j = 0;
+  let cur = chars[0];
+  let counter = 0;
+  for (let i = 0; i <= chars.length; i++) {
+    if (chars[i] === cur) {
+      counter++;
+    } else {
+      chars[j] = cur;
+      if (counter > 1) {
+        const s = counter.toString();
+        for (let k = 0; k < s.length; k++) {
+            chars[++j] = s[k];
+        }
+      }
+      j++;
+      cur = chars[i];
+      counter = 1;
+    }
+  }
+  return j;
+};
