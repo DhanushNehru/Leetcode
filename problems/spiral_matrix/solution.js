46e6d1ba156ccc75d24491189fb02d2a58287d47
Dhanush <dhanushxenocrate@gmail.com>
2023-05-09 14:49:50 +0000

    Sync LeetCode submission - Spiral Matrix (javascript)

---
+++ problems/spiral_matrix/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+    const res = []
+    while(matrix.length){
+        // First row
+        const first = matrix.shift()
+        res.push(...first)
+        
+        for(const m of matrix){
+            let val = m.pop()
+            if(val){
+                res.push(val)
+            }
+            m.reverse()   
+        }   
+        matrix.reverse()
+    }
+    return res
+};
