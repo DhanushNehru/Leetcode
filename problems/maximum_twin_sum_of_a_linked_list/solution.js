251240f311995a8e49a8c1c0719f4f66744898c7
Dhanush <dhanushxenocrate@gmail.com>
2023-05-17 15:40:42 +0000

    Sync LeetCode submission - Maximum Twin Sum of a Linked List (javascript)

---
+++ problems/maximum_twin_sum_of_a_linked_list/solution.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var pairSum = function(head) {
+    var st = []; 
+    while(head != null){
+        st.push(head.val);
+        head = head.next;
+    }
+    let max = 0;
+    for(let i=0; i<st.length; i++){
+        // main logic for getting the twin sum is st[i] + st[st.length-1-i]
+        max = Math.max(max, (st[i] + st[st.length-1-i]));
+    }
+    return max;
+};
