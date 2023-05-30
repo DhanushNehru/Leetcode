ac8b7c50588f4e2ed958f9905bcfbdebde88ea7d
Dhanush <dhanushxenocrate@gmail.com>
2023-05-30 05:45:03 +0000

    Sync LeetCode submission - Design HashSet (python3)

---
+++ problems/design_hashset/solution.py
@@ -0,0 +1,22 @@
+class MyHashSet:
+
+    def __init__(self):
+        # 1000001 = 10^6 + 1
+        self.hashSet = [False]*1000001
+
+    def add(self, key: int) -> None:
+        self.hashSet[key] = True
+
+    def remove(self, key: int) -> None:
+        self.hashSet[key] = False
+
+    def contains(self, key: int) -> bool:
+        return self.hashSet[key]
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
