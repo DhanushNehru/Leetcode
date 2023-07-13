c403256377303e8bf559121cfc2dfcd7cebad85b
Dhanush <dhanushxenocrate@gmail.com>
2023-07-13 06:09:07 +0000

    Sync LeetCode submission - Course Schedule (javascript)

---
+++ problems/course_schedule/solution.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+  // https://en.wikipedia.org/wiki/Topological_sorting
+  const order = [];
+  const queue = [];
+  const graph = new Map();
+  const indegree = Array(numCourses).fill(0);
+
+  for (const [e, v] of prerequisites) {
+    // build graph map
+    if (graph.has(v)) {
+      graph.get(v).push(e);
+    } else {
+      graph.set(v, [e]);
+    }
+    // build indegree array
+    indegree[e]++;
+  }
+
+  for (let i = 0; i < indegree.length; i++) {
+    if (indegree[i] === 0) queue.push(i);
+  }
+
+  while (queue.length) {
+    const v = queue.pop(); // removes last element
+    if (graph.has(v)) {
+      for (const e of graph.get(v)) {
+        indegree[e]--;
+        if (indegree[e] === 0) queue.push(e);
+      }
+    }
+    order.push(v);
+  }
+
+  return numCourses === order.length;
+};
