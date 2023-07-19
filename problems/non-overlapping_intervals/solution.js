e0a9fe721b67b10cf412d367021fb3c7aa5f18b1
Dhanush <dhanushxenocrate@gmail.com>
2023-07-19 06:04:43 +0000

    Sync LeetCode submission - Non-overlapping Intervals (javascript)

--- problems/non-overlapping_intervals/solution.js
+++ problems/non-overlapping_intervals/solution.js
@@ -6,23 +6,16 @@ var eraseOverlapIntervals = function(intervals) {
     // First we sort the intervals based on the ending points
     // [ [ 1, 2 ], [ 2, 3 ], [ 3, 4 ], [ 1, 3 ] ] becomes
     // [ [ 1, 2 ], [ 2, 3 ], [ 1, 3 ], [ 3, 4 ] ]
-    intervals.sort((a,b)=>a[1]-b[1])
-    
-    let noOfIntervalsNotOverlapping = 0;
-    let prev = null;
+    intervals.sort((a,b)=> a[1] - b[1]);
+    let ans = 0, k = null;
     for(let interval of intervals){
-        if(prev == null){
-            prev = interval
-        }
-        else{
-            // the condition to check if it is not overlapping
-            if(prev[1] > interval[0]){
-                ++noOfIntervalsNotOverlapping
-            }
-            else{
-                prev = interval
-            }
-        }
+      let x = interval[0];
+      let y = interval[1];
+      if(k == null || x >= k){
+        k = y;
+      }else{
+        ans++;
+      }
     }
-    return noOfIntervalsNotOverlapping;
+    return ans;
 };
