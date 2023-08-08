310799da9e4d88075fa5e2432ddec5b1529f74a6
Dhanush <dhanushxenocrate@gmail.com>
2023-08-08 07:18:42 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (javascript)

--- problems/search_in_rotated_sorted_array/solution.js
+++ problems/search_in_rotated_sorted_array/solution.js
@@ -4,5 +4,32 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    return nums.indexOf(target)
+    let left = 0, right = nums.length - 1
+    while(left<=right){
+        const mid = Math.floor((left+right) / 2);
+
+        if(nums[mid] === target){
+            return mid;
+        }
+
+        if(nums[left] <= nums[mid]){
+            // target is between mid and left
+            if(nums[left]<=target && target<nums[mid]){
+                right = mid-1
+            }
+            else{
+                left = mid+1
+            }
+        }
+        else{
+            // target is between mid and right
+            if(nums[right]>=target && target>nums[mid]){
+                left = mid+1
+            }
+            else{
+                right = mid-1
+            }
+        }
+    }
+    return -1
 };
