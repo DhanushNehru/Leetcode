428121bbd43f3befbe78aa541754ed10b0a4b397
Dhanush <dhanushxenocrate@gmail.com>
2023-09-07 05:24:59 +0000

    Sync LeetCode submission - Reverse Linked List (javascript)

---
+++ problems/reverse_linked_list/solution.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null, curr = head, next = null;
+    
+    while(curr!==null){
+        next = curr.next
+        curr.next = prev
+
+        prev = curr
+        curr = next
+    }
+    
+    return prev
+};
