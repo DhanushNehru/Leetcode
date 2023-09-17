4fd8739e4ec3c7ec8849602e309dd41e8a8823cd
Dhanush <dhanushxenocrate@gmail.com>
2023-09-17 17:48:15 +0000

    Sync LeetCode submission - Shortest Path Visiting All Nodes (javascript)

---
+++ problems/shortest_path_visiting_all_nodes/solution.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} graph
+ * @return {number}
+ */
+var shortestPathLength = function(graph) {
+    const n = graph.length;
+    const allVisited = (1 << n) - 1;
+    const queue = [];
+    const visited = new Set();
+
+    for (let i = 0; i < n; i++) {
+        queue.push([1 << i, i, 0]);
+        visited.add((1 << i) * 16 + i);
+    }
+
+    while (queue.length > 0) {
+        const [mask, node, dist] = queue.shift();
+
+        if (mask === allVisited) {
+            return dist;
+        }
+
+        for (const neighbor of graph[node]) {
+            const newMask = mask | (1 << neighbor);
+            const hashValue = newMask * 16 + neighbor;
+
+            if (!visited.has(hashValue)) {
+                visited.add(hashValue);
+                queue.push([newMask, neighbor, dist + 1]);
+            }
+        }
+    }
+
+    return -1;
+}
