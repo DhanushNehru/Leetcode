57275632cdd2c46fd81d0fa8ac35ee2506ee2a5c
Dhanush <dhanushxenocrate@gmail.com>
2023-09-28 06:15:12 +0000

    Sync LeetCode submission - Sort Array By Parity (python)

--- problems/sort_array_by_parity/solution.py
+++ problems/sort_array_by_parity/solution.py
@@ -1,4 +1,13 @@
-class Solution:
-    def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        nums.sort(key = lambda n:(n%2, n))
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # Sort the 'nums' list based on a custom key function using the lambda expression.
+        # This key function first groups even and odd numbers (n%2) and then sorts within each group by the actual value (n).
+        nums.sort(key = lambda n: (n % 2, n))
+
+        # Return the sorted 'nums' list.
         return nums
+        
