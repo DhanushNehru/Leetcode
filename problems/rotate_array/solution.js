b3e3e41029f3c6046430f87fdad395e405806962
Dhanush <dhanushxenocrate@gmail.com>
2024-02-13 13:39:58 +0000

    Sync LeetCode submission - Rotate Array (javascript)

--- problems/rotate_array/solution.js
+++ problems/rotate_array/solution.js
@@ -4,8 +4,9 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    const numLength = nums.length;
-    const rightNums = nums.splice(numLength - (k%numLength))
-    console.log(" Right most values ", rightNums)
-    nums.splice(0,0, ...rightNums)
+    // Ensure that the rotation value k is within the range of the array length
+    k = k % nums.length; // Calculate the modulo of k with the length of the array
+
+    // Perform the rotation by removing the last k elements and adding them to the beginning of the array
+    nums.splice(0, 0, ...nums.splice(nums.length - k)); // Remove the last k elements and add them at the beginning
 };
