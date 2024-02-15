6e48a76e9991a81712474f89a03aeaf741a6ced0
Dhanush <dhanushxenocrate@gmail.com>
2024-02-15 05:47:35 +0000

    Sync LeetCode submission - Contains Duplicate II (javascript)

---
+++ problems/contains_duplicate_ii/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const map = new Map()
+
+    for(let i=0;i<nums.length;i++){
+        if(i - map.get(nums[i]) <= k){
+            return true
+        }
+        map.set(nums[i], i)
+    }
+
+    return false;
+};
